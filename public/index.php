3e6b701fa1bf5b3b0c7372d1f68db929987e0f5b
skylying <linsutim@gmail.com>
2014-11-23 12:06:06 +0800

    add web application , index

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Part of achiever project. 
+ *
+ * @copyright  Copyright (C) 2011 - 2014 Achiever, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+include_once __DIR__ . '/../vendor/autoload.php';
+
+(new Achiever\Application\Application)->execute();
---
+++ src/Achiever/Application/Application.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Part of achiever project. 
+ *
+ * @copyright  Copyright (C) 2011 - 2014 Achiever, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace Achiever\Application;
+
+
+use Joomla\Application\AbstractWebApplication;
+
+class Application extends AbstractWebApplication
+{
+
+    /**
+     * Method to run the application routines.  Most likely you will want to instantiate a controller
+     * and execute it, or perform some sort of task directly.
+     *
+     * @return  void
+     *
+     * @since   1.0
+     */
+    protected function doExecute()
+    {
+        echo 'This is first step of achiever!';
+    }
+}
+ 
