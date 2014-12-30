8d7794514b31fff503081f656d4b2983845ae1a4
skylying <linsutim@gmail.com>
2014-12-30 14:25:01 +0800

    add exercise model and view

---
+++ src/Achiever/Model/ExerciseModel.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Part of achiever project. 
+ *
+ * @copyright  Copyright (C) 2011 - 2014 Achiever, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace Achiever\Model;
+
+
+use Joomla\Model\AbstractModel;
+
+class ExerciseModel extends AbstractModel
+{
+    // tbd
+}
+ 
