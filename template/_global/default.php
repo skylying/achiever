48c1434a8ee4b733accd83aae5eb9ff2023a401d
skylying <linsutim@gmail.com>
2015-01-02 13:38:26 +0800

    add bootstrap library

--- template/_global/default.php
+++ template/_global/default.php
@@ -2,4 +2,36 @@
 
 ?>
 
-<h1>Hello world, this is default global template</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link rel="icon" href="../../favicon.ico">
+
+	<title>Index Template</title>
+
+	<link rel="stylesheet" href="media/library/bootstrap/css/bootstrap.min.css" />
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+
+<body>
+
+<div class="container">
+	<h1>Hello world</h1>
+</div>
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="media/library/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
